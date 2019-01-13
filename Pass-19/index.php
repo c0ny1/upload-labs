26a605a2cef49da3bfac31a91479fa776b6bcd3c
c0ny1 <1627639645@qq.com>
2019-01-14 00:41:33 +0800

    解决Pass-19 Linux下无法使用CVE-2017-15715绕过上传限制问题

--- Pass-19/index.php
+++ Pass-19/index.php
@@ -10,12 +10,17 @@ if (isset($_POST['submit'])) {
     if (file_exists(UPLOAD_PATH)) {
         $deny_ext = array("php","php5","php4","php3","php2","html","htm","phtml","pht","jsp","jspa","jspx","jsw","jsv","jspf","jtml","asp","aspx","asa","asax","ascx","ashx","asmx","cer","swf","htaccess");
 
+        /*
         $file_name = trim($_POST['save_name']);
         $file_name = deldot($file_name);//删除文件名末尾的点
         $file_ext = pathinfo($file_name,PATHINFO_EXTENSION);
         $file_ext = strtolower($file_ext); //转换为小写
         $file_ext = str_ireplace('::$DATA', '', $file_ext);//去除字符串::$DATA
         $file_ext = trim($file_ext); //首尾去空
+        */
+
+        $file_name = $_POST['save_name'];
+        $file_ext = pathinfo($file_name,PATHINFO_EXTENSION);
 
         if(!in_array($file_ext,$deny_ext)) {
             $temp_file = $_FILES['upload_file']['tmp_name'];

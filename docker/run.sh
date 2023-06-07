5d327f8728456ea21f651634bb83f60b21ea4bbb
PHAN HOANG NAM <61881881+namhikelo@users.noreply.github.com>
2023-06-07 14:58:31 +0700

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,13 @@
+#Download Docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install docker-ce
+
+#Download Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+##Run dockerfile upload-labs
 docker build -t upload-labs .
 docker run -d -p 80:80 upload-labs:latest
